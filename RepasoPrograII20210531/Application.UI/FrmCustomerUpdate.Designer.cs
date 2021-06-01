25805d8cc8f012fc7e5d6237eed42e5d6cbe1ff8
adriana elena reynaldi <magaligracia@gmail.com>
2021-05-31 22:05:24 -0300

    repaso

---
+++ RepasoPrograII20210531/Application.UI/FrmCustomerUpdate.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrmCustomerUpdate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.TabIndex = 4;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.TabIndex = 5;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtAge.TabIndex = 3;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(16, 343);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(283, 28);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // FrmCustomerUpdate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 398);
+            this.Controls.Add(this.btnUpdate);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "FrmCustomerUpdate";
+            this.Text = "FrmCustomerUpdate";
+            this.Controls.SetChildIndex(this.btnUpdate, 0);
+            this.Controls.SetChildIndex(this.btnAdd, 0);
+            this.Controls.SetChildIndex(this.btnCancel, 0);
+            this.Controls.SetChildIndex(this.lblName, 0);
+            this.Controls.SetChildIndex(this.lblLastName, 0);
+            this.Controls.SetChildIndex(this.txtLastName, 0);
+            this.Controls.SetChildIndex(this.txtName, 0);
+            this.Controls.SetChildIndex(this.txtAge, 0);
+            this.Controls.SetChildIndex(this.lblAge, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
---
+++ RepasoPrograII20210531/Application/Repositories/CustomerRepository.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Application.Exceptions;
+using Application.DataAcces;
+using Application.Models;
+
+namespace Application.Repositories
+{
+    public class CustomerRepository : RepositoryBase<Customer>
+    {
+        private static List<Customer> customers;
+
+        #region -- Los datos no se encuentran guardados más que en memoria --
+
+        static CustomerRepository()
+        {
+            // Carga de 100 clientes de forma predeterminada
+            customers = new List<Customer>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var customer = new Customer();
+                customer.Id = i + 1;
+                customer.Name = "CustomerName" + customer.Id;
+                customer.LastName = "CustomerLastname" + customer.Id;
+                customer.Age = 20 + (int)customer.Id;
+
+                customers.Add(customer);
+            }
+        }
+
+        #endregion
+
+        public override void Create(Customer entity)
+        {
+            try
+            {
+                long lastId = (long)(customers[customers.Count - 1]).Id;
+                entity.Id = lastId + 1;
+                if (customers != entity)
+                {
+                    customers.Add(entity);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new TechnicalException(
+                    string.Format("No se pudo crear el cliente \"{0} {1}\".", entity.Name, entity.LastName),
+                    ex);
+            }
+
+        }
+
+        public override List<Customer> GetAll()
+        {
+            // Devuelve una nueva lista ordenada
+            //TODO: resolver el error
+            List<Customer> customers2 = new List<Customer>();
+            
+            
+
+            return customers2.ToList();
+        }
+        public List<Customer> GetAll(string path)
+        {
+            CustomerSerializer customerSerializer = new CustomerSerializer();
+            customers.AddRange(customerSerializer.Read(path));
+            return customers;
+        }
+
+        public override Customer GetById(long entityId)
+        {
+            foreach(Customer customer in customers)
+            {
+                if(customer.Id == entityId)
+                {
+                    return customer;
+                }
+            }
+            return null;
+        }
+
+        public override void Remove(Customer entity)
+        {
+            if (customers.Contains(entity))
+            {
+                customers.Remove(entity);
+            }
+        }
+
+        public override void Update(Customer entity)
+        {
+            foreach (Customer customer in customers)
+            {
+                if (customer.Id == entity.Id)
+                {
+                    
+                }
+            }
+        }
+
+        public List<Customer> LoadFromFile(string path)
+        {
+
+            CustomerSerializer customerSerializer = new CustomerSerializer();
+            customers.AddRange(customerSerializer.Read(path));
+            return customers;
+        }
+
+        public bool SaveToFile(List<Customer> customers)
+        {
+            CustomerSerializer customerSerializer = new CustomerSerializer();
+            return customerSerializer.Save(customers);
+        }
+
+    }
+}
